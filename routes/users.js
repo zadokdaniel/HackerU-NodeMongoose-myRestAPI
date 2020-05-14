1813106345ddc481d3bd18691fde1a6393ebbcd8
Daniel Zadok <zadok.daniel@gmail.com>
2020-05-14 15:45:32 +0300

    11:save user

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,23 @@ const route = express.Router();
 const { User, validate } = require("../models/user");
 
 route.post("/", async (req, res) => {
+  // validate body data
   const { error } = validate(req.body);
+
+  // in case body data is invalid send error message
   if (error) {
-    console.log(error);
     return res.status(400).send(error.details[0].message);
   }
 
-  const user = await User.findOne({ email: req.body.email });
+  // validate user email doesn't exist
+  let user = await User.findOne({ email: req.body.email });
+
+  // in case user email already exists send error message
   if (user) {
     return res.status(400).send("User already exists!");
   }
+
+  res.send("ok");
 });
 
 module.exports = route;
